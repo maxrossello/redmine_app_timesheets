2c3f485f1b6b388be4bd15d3c296a85c386fa58b
Massimo Rossello <massimo.rossello@azcom.it>
2014-11-26 09:53:07 +0100

    Order view accessibility does not need to be visible in app menu

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -13,7 +13,7 @@ class OrdersController < ApplicationController
   end
 
   def index
-    unless User.is_app_visible?('order_mgmt')
+    unless User.is_app_enabled?('order_mgmt')
       render_404
     else
       @ts_project = Setting.plugin_redmine_app_timesheets['project'].to_i
