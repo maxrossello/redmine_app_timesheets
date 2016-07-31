e1e494dde6f9f817a85060074f2d5488dc74342d
Massimo Rossello <massimo.rossello@azcom.it>
2016-08-01 01:25:45 +0200

    Do not raise validation error if activity is unlinked. fixes #78

--- app/models/ts_time_entry.rb
+++ app/models/ts_time_entry.rb
@@ -3,4 +3,11 @@ class TsTimeEntry < TimeEntry
   belongs_to :order, :class_name => 'WorkOrder', :foreign_key => 'order_id'
 
   default_scope { where("#{TsTimeEntry.table_name}.order_id IS NOT NULL") }
+
+  def validate_time_entry
+    super
+    # do not complain for locally overridden activities
+    errors.delete :activity_id if activity_id_changed? && project && project.activities.map(&:parent).compact.include?(activity)
+  end
+
 end
